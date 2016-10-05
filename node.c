94c2874d6f7c9825b531f9a70da1bd8cb9e5ca19
bervianto <13514047@std.stei.itb.ac.id>
2016-10-05 17:32:30 +0700

    copy

--- node.c
+++ node.c
@@ -9,6 +9,8 @@
 #include <errno.h>
 #include <fcntl.h> // for open
 #include <unistd.h> // for close
+#include <string.h>
+#include "getip.c"
 
 #define SERVER_PORT 4949
 
@@ -18,17 +20,26 @@ int main()
 	struct sockaddr_in sin;
 	//socket
 	int sock_listen;
+    int yes = 1;
+    char * ip;
+    ipv4(&ip);
+    printf("%s",ip);
 	//address
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(SERVER_PORT);
-	sin.sin_addr.s_addr = inet_addr("167.205.3.165");
+	sin.sin_addr.s_addr = inet_addr(ip);
 	if((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 		printf("Error Create Socket\n");
 		exit(errno);
 	}
 
-	if(bind(sock_listen, (struct sockaddr*) &sin, sizeof(sin))<0){
-		printf("error binding");
+    if (setsockopt(sock_listen, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+	
+    if(bind(sock_listen, (struct sockaddr*) &sin, sizeof(sin))<0){
+		printf("error binding\n");
 		exit(errno);
 	}
 	
@@ -39,11 +50,14 @@ int main()
 
 	while(1){
 		char buf[256];
-		recv(client_accept, buf, sizeof(buf),0);
-		printf("%s",buf);
+        recv(client_accept, buf, sizeof(buf),0);
+		if(buf>0){
+            
+        }
 	}
-
+    
 	close(sock_listen);
 
 	return 0;
 }
+
